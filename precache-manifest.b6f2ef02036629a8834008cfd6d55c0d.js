e455cbba449d03af6af5bf5436665d4583a2e955
Deyan Angelov <didkobravo@mac.lan>
2025-01-15 22:33:37 +0200

    Updates

---
+++ precache-manifest.b6f2ef02036629a8834008cfd6d55c0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e4d4c8a867787b0bd15ac0758b92f9",
+    "url": "/presentation-timer/index.html"
+  },
+  {
+    "revision": "28e4a699624610465b59",
+    "url": "/presentation-timer/static/css/2.35687568.chunk.css"
+  },
+  {
+    "revision": "1999bf9fe3681b7fb021",
+    "url": "/presentation-timer/static/css/main.4b161d75.chunk.css"
+  },
+  {
+    "revision": "28e4a699624610465b59",
+    "url": "/presentation-timer/static/js/2.44032ac0.chunk.js"
+  },
+  {
+    "revision": "d17bf58ce29c21b922f6",
+    "url": "/presentation-timer/static/js/3.d2763252.chunk.js"
+  },
+  {
+    "revision": "1999bf9fe3681b7fb021",
+    "url": "/presentation-timer/static/js/main.7cdc4a22.chunk.js"
+  },
+  {
+    "revision": "e1d58d8720c5d99d9195",
+    "url": "/presentation-timer/static/js/runtime~main.c3430a11.js"
+  }
+]);
