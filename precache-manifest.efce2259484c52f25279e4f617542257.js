f1a1ddfd9e2db02f8705468a72963f627db09fd8
Deyan Angelov <didkobravo@deyans-air.lan>
2025-01-16 18:07:35 +0200

    Updates

--- precache-manifest.efce2259484c52f25279e4f617542257.js
+++ precache-manifest.efce2259484c52f25279e4f617542257.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "987561c9845d5301919f00a99d6661f7",
+    "revision": "21b3459cdbfc3b35a039b03519704cc0",
     "url": "/presentation-timer/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/presentation-timer/static/css/2.35687568.chunk.css"
   },
   {
-    "revision": "caef5266f149f7de9038",
+    "revision": "7bb8d753646b1e90da5e",
     "url": "/presentation-timer/static/css/main.4b161d75.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/presentation-timer/static/js/3.760a3ae0.chunk.js"
   },
   {
-    "revision": "caef5266f149f7de9038",
-    "url": "/presentation-timer/static/js/main.8bff5a6d.chunk.js"
+    "revision": "7bb8d753646b1e90da5e",
+    "url": "/presentation-timer/static/js/main.4298be5e.chunk.js"
   },
   {
     "revision": "02a5ea3f471f1af2f8ca",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/presentation-timer/precache-manifest.d19867160b6610660b513af58b4ec273.js"
+  "/presentation-timer/precache-manifest.efce2259484c52f25279e4f617542257.js"
 );
 
 self.addEventListener('message', (event) => {
